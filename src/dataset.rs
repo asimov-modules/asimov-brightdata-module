86da836ba1558250ebf91395a1a115eceb1ccc95
AlexSanches1 <sanches2026@gmail.com>
2025-06-30 22:07:47 -0400

    Implement Default trait for Dataset with explicit implementation

--- src/dataset.rs
+++ src/dataset.rs
@@ -2,6 +2,7 @@
 
 use crate::jq::JsonFilter;
 
+#[cfg(feature = "std")]
 #[derive(Clone, Debug)]
 pub struct Dataset {
     pub id: &'static str,
@@ -9,6 +10,27 @@ pub struct Dataset {
     pub jq_filter: fn() -> &'static JsonFilter,
 }
 
+#[cfg(not(feature = "std"))]
+#[derive(Clone, Debug)]
+pub struct Dataset {
+    pub id: &'static str,
+    pub label: &'static str,
+    pub jq_filter: fn() -> JsonFilter,
+}
+
+#[cfg(feature = "std")]
+impl Default for Dataset {
+    fn default() -> Self {
+        use crate::jq;
+        Self {
+            id: "",
+            label: "",
+            jq_filter: jq::default_filter,
+        }
+    }
+}
+
+#[cfg(not(feature = "std"))]
 impl Default for Dataset {
     fn default() -> Self {
         use crate::jq;
